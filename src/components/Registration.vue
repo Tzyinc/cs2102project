6832137f8e316244748fdf5444a3961d14b8d00a
qy <qinying1313@yahoo.com>
2017-09-28 23:31:13 +0800

    registration ajax version
    $http version not working for now

--- src/components/Registration.vue
+++ src/components/Registration.vue
@@ -1,24 +1,61 @@
 <template>
-  <div class="Registration">
-    <h1>Register</h1>
-  </div>
-</template>
+    <div class="col-sm-4 col-sm-offset-4">
+      <h2>Register</h2>
+      <div class="alert alert-danger" v-if="error">
+        <p>{{ error }}</p>
+      </div>
+      <div class="form-group">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Username"
+          v-model="credentials.username"
+        >
+      </div>
+      <div class="form-group">
+        <input
+          type="password"
+          class="form-control"
+          placeholder="Password"
+          v-model="credentials.password"
+        >
+      </div>
+      <button class="btn btn-primary" @click="submit()">Submit</button>
+	  <p style="color:red">{{register_msg}}</p>
+    </div>
+  </template>
 
-<script>
-export default {
-
-  data () {
-  	return{
-  		
-  	}
-    /* {
-      source: ""
-    }*/
+  <script>
+  import auth from '../auth/auth'
+  export default {
+    data() {
+      return {
+        // We need to initialize the component with any
+        // properties that will be used in it
+        credentials: {
+          username: '',
+          password: ''
+        },
+		register_msg: '',
+        error: ''
+      }
+    },
+    methods: {
+      submit() {
+		
+        var credentials = {
+          username: this.credentials.username,
+          password: this.credentials.password
+        }
+        // We need to pass the component's this context
+        // to properly make use of http in the auth service
+		//this.$http.put('http://localhost:8080/api/user', credentials)
+		//	.then(response => {
+		//	register_msg = JSON.parse(response);
+		//	});
+  
+        this.register_msg = auth.signup(this, credentials, '/')
+      }
+    }
   }
-}
-</script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
-</style>
+  </script>
