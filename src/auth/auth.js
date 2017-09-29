5d6b25e160e3a0280e5bcaa007504685446d62d7
Tzyinc <tzyinc@hotmail.com>
2017-09-29 14:08:49 +0800

    Added external api source

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -1,12 +1,8 @@
 import router from '@/router'
-
+import api_ep from '../api.json'
 // URL and endpoint constants
-const API_URL = 'http://localhost:8080/'
-const LOGIN_URL = API_URL + 'api/user'
-const SIGNUP_URL = API_URL + 'api/user'
 
 export default {
-
   // User object will let us check authentication status
   user: {
     authenticated: false
@@ -14,34 +10,39 @@ export default {
 
   // Send a request to the login URL and save the returned credentials
   login(context, creds, redirect) {
-    context.$http.post(LOGIN_URL, creds)
-	  .then(response => {
-      if(response.login_result == true) {
-		localStorage.setItem('id_token', response.id_token)
-		localStorage.setItem('access_token', response.access_token)
+    context.$http
+      .post(api_ep.API_URL + api_ep.USER, creds)
+      .then(response => {
+        if (response.login_result === true) {
+          localStorage.setItem('id_token', response.id_token)
+          localStorage.setItem('access_token', response.access_token)
 
-	  
-	  this.user.authenticated = true
-	  // Redirect to a specified route
-	  if(redirect) {
-		router.go(redirect)        
-	  }
-	  return '';
-	} else {
-		return 'Incorrect username/password!';
-	}
-
-    }).error((err) => {
-      context.error = err
-    })
+          this.user.authenticated = true
+          // Redirect to a specified route
+          if (redirect) {
+            router.go(redirect)
+          }
+          return ''
+        } else {
+          return 'Incorrect username/password!'
+        }
+      })
+      .error(err => {
+        context.error = err
+      })
   },
 
   signup(context, creds, redirect) {
-	  $.ajax({ url: SIGNUP_URL, //Your api url 
-	  type: 'PUT', //type is any HTTP method 
-	  data: { data: creds }, //Data as js object 
-	  success: function () { console.log('test'); return data;} }) ;
-  //console.log('Signed up');
+    $.ajax({
+      url: api_ep.API_URL + api_ep.USER, //Your api url
+      type: 'PUT', //type is any HTTP method
+      data: {data: creds}, //Data as js object
+      success: function(){
+        console.log('test')
+        return data
+      }
+    })
+    //console.log('Signed up');
     /*context.$http.put(SIGNUP_URL, creds, (data) => {
       /*localStorage.setItem('id_token', data.id_token)
       localStorage.setItem('access_token', data.access_token)
@@ -49,7 +50,7 @@ export default {
       this.user.authenticated = true
 
       if(redirect) {
-        router.go(redirect)        
+        router.go(redirect)
       }
 		console.log('Signed up');
 		return JSON.parse(data);
@@ -65,18 +66,17 @@ export default {
 
   checkAuth() {
     var jwt = localStorage.getItem('id_token')
-    if(jwt) {
+    if (jwt) {
       this.user.authenticated = true
-    }
-    else {
-      this.user.authenticated = false      
+    } else {
+      this.user.authenticated = false
     }
   },
 
   // The object to be passed as a header for authenticated requests
   getAuthHeader() {
     return {
-      'Authorization': 'Bearer ' + localStorage.getItem('access_token')
+      Authorization: 'Bearer ' + localStorage.getItem('access_token')
     }
   }
 }
