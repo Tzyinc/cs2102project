56e2c0afcb2b57b15fb5fce477bfd3f26856553e
YJ <leeyingjie_95@hotmail.com>
2017-10-30 12:01:50 +0800

    changed date display

--- src/components/ItemOwnerInfo.vue
+++ src/components/ItemOwnerInfo.vue
@@ -8,10 +8,10 @@
       <p><span class="item-info-attr"><i class="fa fa-gift" aria-hidden="true"></i> Name</span><br/><span class="item-info-value">{{name}}</span></p><hr/>
       <p><span class="item-info-attr"><i class="fa fa-user" aria-hidden="true"></i> Owner</span><br/><span class="item-info-value">
               <router-link :to="{name: 'MyProfile', params: {uid: this.owner_username}}">{{owner_username}}</router-link></span></p><hr/>
-      <p><span class="item-info-attr"><i class="fa fa-clock-o" aria-hidden="true"></i> Time Listed</span><br/><span class="item-info-value">{{timelisted}}</span></p><hr/>
+      <p><span class="item-info-attr"><i class="fa fa-clock-o" aria-hidden="true"></i> Date Listed</span><br/><span class="item-info-value">{{parseDate(timelisted)}}</span></p><hr/>
       <p><span class="item-info-attr"><i class="fa fa-map-marker" aria-hidden="true"></i> Location</span><br/><span class="item-info-value">{{location}}</span></p><hr/>
-      <p><span class="item-info-attr"><i class="fa fa-calendar-check-o" aria-hidden="true"></i> Available start</span><br/><span class="item-info-value">{{startdate}}</span></p><hr/>
-      <p><span class="item-info-attr"><i class="fa fa-calendar-times-o" aria-hidden="true"></i> Available end</span><br/><span class="item-info-value">{{enddate}}</span></p>
+      <p><span class="item-info-attr"><i class="fa fa-calendar-check-o" aria-hidden="true"></i> Available start</span><br/><span class="item-info-value">{{parseDate(startdate)}}</span></p><hr/>
+      <p><span class="item-info-attr"><i class="fa fa-calendar-times-o" aria-hidden="true"></i> Available end</span><br/><span class="item-info-value">{{parseDate(enddate)}}</span></p>
     </div>
   </div>
 </template>
@@ -25,6 +25,54 @@ export default {
     return {
       item: {
       }
+    }
+  },
+  methods: {
+    parseDate(str) {
+      var year = str.substring(0,4)
+      var month = str.substring(5,7)
+      var day = str.substring(8,10)
+
+      switch(month) {
+        case '01':
+          month = 'Jan'
+          break
+        case '02':
+          month = 'Feb'
+          break
+        case '03':
+          month = 'Mar'
+          break
+        case '04':
+          month = 'Apr'
+          break
+        case '05':
+          month = 'May'
+          break
+        case '06':
+          month = 'Jun'
+          break
+        case '07':
+          month = 'Jul'
+          break
+        case '08':
+          month = 'Aug'
+          break
+        case '09':
+          month = 'Sep'
+          break
+        case '10':
+          month = 'Oct'
+          break
+        case '11':
+          month = 'Nov'
+          break
+        case '12':
+          month = 'Dec'
+          break
+      }
+
+      return day + " " + month + " " + year
     }
   }
 }
