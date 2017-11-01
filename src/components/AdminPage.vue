29301f123d14ae73db3d2422a9798d621c8a8544
YJ <leeyingjie_95@hotmail.com>
2017-11-01 21:16:11 +0800

    add admin page skeleton

---
+++ src/components/AdminPage.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AdminPage',
+
+
+  data() {
+    return {
+      login_user: '',
+    }
+  },
+
+  created: function() {
+
+  }
+
+}
+
+</script>
+
+<style scoped>
+
+</style>
