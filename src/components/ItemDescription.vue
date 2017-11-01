7814ba32c10fa4e0aa9e49a5770056c2f6a2729c
YJ <leeyingjie_95@hotmail.com>
2017-11-01 15:08:13 +0800

    fix undefined tags

--- src/components/ItemDescription.vue
+++ src/components/ItemDescription.vue
@@ -20,7 +20,7 @@ export default {
   props: ['description', 'tags'],
   data () {
     return {
-      tags,
+      tags: '',
       item: {
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         tags: 'abc, def, ghi'
