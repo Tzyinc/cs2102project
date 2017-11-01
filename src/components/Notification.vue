43dc427f5abf66b7b1cb208daef222cc7e35dee9
Qin Ying <qinying1313@yahoo.com>
2017-11-01 13:14:25 +0800

    Notification fixed
    notification fixed

--- src/components/Notification.vue
+++ src/components/Notification.vue
@@ -1,7 +1,7 @@
 <template>
-<div class="list-group-item notification" v-bind:class="{ active: !read }" v-on:mouseover="markNotiRead(nid, isRead)">
+<div class="list-group-item notification" v-bind:class="{ active: !read }" v-on:mouseover="markNotiRead(nid, isRead)" v-on:click="goToItem(iid)">
 	<div v-if="type='bidMade'">
-        New bid on '{{iid}}'
+        New bid on '{{name}}'
     </div>
 </div>
 </template>
@@ -15,7 +15,7 @@ var api_notiId = '?nid=';
 
 export default {
 	name: 'Notification',
-  	props: ['iid', 'notiType', 'isRead','nid'],
+  	props: ['iid', 'notiType', 'isRead','nid', 'name'],
  	 data () {
     	return {
     		read : ''
@@ -24,15 +24,13 @@ export default {
   	methods: {
   		markNotiRead (notiId, read) {
         	if(!this.read) {
-        		console.log(notiId + " read!")
-						alert(notiId)
         		this.read = true;
 
 	        	$.ajax({
 	                url: api_url_noti, //Your api url
 	                type: 'POST', //type is any HTTP method
 	                headers: auth.getAuthHeader(this),
-									data: {data: {"nid": notiId}},
+					data: {data: {"nid": notiId}},
 	                success: function(response){
 	                    if(response.success) {
 	                		console.log("Notification mark as read")
@@ -41,6 +39,9 @@ export default {
 	            })
 	        }
 
+    	},
+    	goToItem(iid) {
+    		this.$router.push({ name: 'DetailedItem', params: { iid: iid }})
     	}
   	},
   	watch: {
